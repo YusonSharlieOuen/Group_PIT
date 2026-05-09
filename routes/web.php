4fd93d9e2abd761b6711eb7d7980e9f64455d181
YusonSharlieOuen <yuson.sharlieouen@gmail.com>
2026-05-09 08:13:16 +0800

    Added one mcr and one page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\UserProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,6 +31,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/user_profile', [UserProfileController::class, 'index'])->name('user_profile.index');
 });
 
 require __DIR__.'/auth.php';
